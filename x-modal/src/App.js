55d69d5e332af71a9e17a8047bdbde965f88b93c
Dibya Ranjan sahoo <ranjandibya34512@gmail.com>
2025-01-13 13:47:52 +0530

    add

--- x-modal/src/App.js
+++ x-modal/src/App.js
@@ -1,11 +1,42 @@
 
-import './App.css';
-import XModal from './Components/XModal';
+// import './App.css';
+// import XModal from './Components/XModal';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <XModal/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState } from "react";
+import Modal from "./Components/XModal";
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+  const handleopen = () => {
+    setOpenModal(true);
+  };
   return (
-    <div className="App">
-      <XModal/>
+    <div className="main">
+      <h1>User Details Modal</h1>
+      <button
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          border: "none",
+          padding: "1rem",
+          borderRadius: "10px",
+          fontSize: "larger",
+        }}
+        onClick={handleopen}
+      >
+        Open Form
+      </button>
+      <div>{openModal && <Modal closeModal={setOpenModal} />}</div>
     </div>
   );
 }
